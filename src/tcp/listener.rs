3df0f496c8453158a9ccc593560e87861401f08e
Alexander Smirnov <asmirn01t@knowledgenettest.onmicrosoft.com>
2024-02-20 15:16:24 +0700

    updates

--- src/tcp/listener.rs
+++ src/tcp/listener.rs
@@ -1,8 +1,11 @@
 use crate::{config::ConfigInner, tcp::process::process_tcp};
 use anyhow::Result;
 use rustls::ServerConfig;
-use std::sync::Arc;
-use tokio::net::{TcpListener, TcpStream};
+use std::{future::Future, sync::Arc};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    task,
+};
 use tokio_rustls::TlsAcceptor;
 
 pub struct PtmListener {
@@ -34,22 +37,14 @@ impl PtmListener {
             if tls_detected(&incoming).await? {
                 match acceptor.accept(incoming).await {
                     Ok(incoming) => {
-                        tokio::spawn(async move {
-                            if let Err(e) = process_tcp(incoming, config_clone).await {
-                                tracing::error!("{e}");
-                            }
-                        });
+                        spawn_and_log_error(process_tcp(incoming, config_clone));
                     }
                     Err(e) => {
                         tracing::error!("Failed to accept tls stream: {e}");
                     }
                 }
             } else {
-                tokio::spawn(async move {
-                    if let Err(e) = process_tcp(incoming, config_clone).await {
-                        tracing::error!("{e}");
-                    }
-                });
+                spawn_and_log_error(process_tcp(incoming, config_clone));
             }
         }
 
@@ -63,15 +58,22 @@ impl PtmListener {
         while let Ok((incoming, _)) = listener.accept().await {
             let config_clone = self.config.clone();
 
-            tokio::spawn(async move {
-                if let Err(e) = process_tcp(incoming, config_clone).await {
-                    tracing::error!("{e}");
-                }
-            });
+            spawn_and_log_error(process_tcp(incoming, config_clone));
         }
 
         Ok(())
     }
+}
+
+fn spawn_and_log_error<F>(f: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<()>> + Send + 'static,
+{
+    tokio::spawn(async move {
+        if let Err(e) = f.await {
+            tracing::error!("{e}");
+        }
+    })
 }
 
 async fn tls_detected(stream: &TcpStream) -> Result<bool> {
